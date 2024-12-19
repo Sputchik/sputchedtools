3762afbef2d2cdcb9bd02fd82297da1e9c5df8d6
Sputchik <sputchik@gmail.com>
2024-12-19 17:26:22 +0100

    0.22.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = 'sputchedtools',
-	version = '0.22.1',
+	version = '0.22.2',
 	packages = find_packages(),
 	py_modules = ['sputchedtools'],
 	install_requires = [
